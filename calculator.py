b43a37d2517d7787420795025f279257a3de27fd
Stav Shukrun <shukrunstav@gmail.com>
2021-11-25 20:03:00 +0200

    thread class

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+""" Main """
 import argparse
 import threading
 import time
@@ -10,32 +11,23 @@ def create_parser():
     return args
 
 def take_expresion(file_name: str) -> str:
-    try: 
-        with open(file_name,'r') as f:
-            return f.read()
+    try:
+        with open(file_name,'r') as expresion:
+            return expresion.read()
 
-    except (FileNotFoundError):
-        raise
+    # except (FileNotFoundError):
+    #     raise FileNotFoundError("please check File")
     except (TypeError):
         print("write your expression:")
-        equation = input("> ") 
+        equation = input("> ")
         return equation
 
-def count_calls(operation):
+def count_operations(operation):
     def _counting(*args, **kwargs):
         _counting.count_operation += 1
         return operation(*args, **kwargs)
     _counting.count_operation = 0
     return _counting
-
-def thread_additions():
-    t = threading.currentThread()
-    while getattr(t, "do_run", True):
-        cal =Calculator
-        number_of_addition = cal.add.count_operation # pylint: disable=no-member
-        print("background thread number of additions ",number_of_addition)
-        # threading.Timer(0.1, thread_additions).start()
-        time.sleep(0.1)
 
 class Calculator():
 
@@ -90,47 +82,62 @@ class Calculator():
     def sum(self,stack: list) -> float:
 
         round_val = 4
-        sum = stack[0]
+        sum_in_stack = stack[0]
         for num in range(len(stack)-1):
             if stack[num+1] > 0:
-                sum = self.add(sum,stack[num+1])
+                sum_in_stack = self.add(sum_in_stack,stack[num+1])
             elif stack[num+1] < 0:
-                sum = self.subtract(sum,stack[num+1])
-        return round(sum,round_val)
+                sum_in_stack = self.subtract(sum_in_stack,stack[num+1])
+        return round(sum_in_stack,round_val)
 
-    @count_calls
+    @count_operations
     def add(self,a: float,b: float) -> float:
         a+=b
 
         return a    
 
-    @count_calls
+    @count_operations
     def subtract(self,a: float,b: float) -> float:
         a+=b
         return a    
 
-    @count_calls
+    @count_operations
     def multiply(self,a: float,b: float) -> float:
         a=a*b
         return a
 
-    @count_calls
+    @count_operations
     def divide(self,a: float,b: float) -> float:
         if(b==0):
             raise ZeroDivisionError("can't divided by zero")
         a=a/b
         return a
 
+class BackgrundThreadAdditions(threading.Thread):
+    """"Thread Class"""
+    def __init__(self, interval:int =1) -> None:
+        threading.Thread.__init__(self)
+        self.interval = interval
+
+    def run(self):
+        cal = Calculator
+        while True:
+            number_of_addition =cal.add.count_operation
+            print("background thread number of additions ",number_of_addition)
+            time.sleep(self.interval)
 
 def start_calculator():
-    t = threading.Thread(target=thread_additions,)
-    t.start()
+    new_thread = BackgrundThreadAdditions()
+    new_thread.start()
     cal= Calculator()
     args = create_parser()
     salusion=take_expresion(args.f)
     print(cal.calculate(salusion))
-    print(f" Total operations: {cal.add.count_operation + cal.subtract.count_operation + cal.multiply.count_operation + cal.divide.count_operation} \n Add operations: {cal.add.count_operation} \n Subtract operations: {cal.subtract.count_operation} \n Multiply operations: {cal.multiply.count_operation} \n Divide operations: {cal.divide.count_operation} \n") # pylint: disable=no-member
-    t.do_run = False
+    print(f"""    Total operations: {cal.add.count_operation + cal.subtract.count_operation + cal.multiply.count_operation + cal.divide.count_operation}
+    Add operations: {cal.add.count_operation}
+    Subtract operations: {cal.subtract.count_operation}
+    Multiply operations: {cal.multiply.count_operation}
+    Divide operations: {cal.divide.count_operation} """)
 
 if __name__ == '__main__':
     start_calculator()
